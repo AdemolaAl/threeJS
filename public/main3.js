40ac81f6a03ada46c3785daab460f79f65d72cf9
Ademola Al-ameen <ademolaalameen86@gmail.com>
2023-08-28 12:20:56 +0100

    good

--- public/main3.js
+++ public/main3.js
@@ -1,15 +1,23 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 10);
+renderer.setClearColor(0xFFFFFF);
+renderer.setPixelRatio(2)
+
+
+
+//lighting
+var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 10);
 directionalLight.position.set(1, 1, 1);
 scene.add(directionalLight);
+
 
 const loader = new GLTFLoader();
 loader.load('public/free_-_fantasy_town/scene.gltf', function (gltf) {
@@ -19,6 +27,7 @@ loader.load('public/free_-_fantasy_town/scene.gltf', function (gltf) {
 }, undefined, function (error) {
     console.error(error);
 });
+
 
 camera.position.z = 5;
 camera.position.y = 1;
